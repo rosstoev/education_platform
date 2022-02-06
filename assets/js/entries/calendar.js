0f48634afd20ec6187de01e4103a5bc3f578dfc5
ros <rossto3v@gmail.com>
2022-02-06 11:21:59 +0200

    Init calendar

--- assets/js/entries/calendar.js
+++ assets/js/entries/calendar.js
@@ -1,9 +1,24 @@
+import bgLocale from "@fullcalendar/core/locales/bg";
 
-$(document).ready(function (){
-    const calendarComponent = $("#calendar");
+const calendarEl = document.getElementById("calendar");
+const calendar = new FullCalendar(calendarEl, {
+        plugins: [FullCalendarDayGridPlugin, FullCalendarTimeGridPlugin, FullCalendarListPlugin, FullCalendarInteractionPlugin],
+        locales: [bgLocale], // english is default and is built-in
+        locale: 'bg',
+        firstDay: 1,
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,dayGridDay'
+        },
+        views: {
+            dayGridMonth: {
+                dayMaxEventRows: 3 // adjust to 6 only for timeGridWeek/timeGridDay
+            }
+        },
+        events: calendarEvents
+    }
+);
 
-    var calendar = new FullCalendar.Calendar(calendarComponent, {
-        initialView: 'dayGridMonth'
-    });
-    calendar.render();
-});
+calendar.render();
--- assets/js/entries/libraries.js
+++ assets/js/entries/libraries.js
@@ -4,5 +4,21 @@ window.$ = $;
 
 import "bootstrap";
 import "select2";
-import "admin-lte";
-import 'fullcalendar/main.min';
+
+import * as moment from 'moment';
+window.moment = moment;
+
+// import "admin-lte/plugins/fullcalendar/main.min";
+// import 'fullcalendar/main.min';
+
+import {Calendar} from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+window.FullCalendar = Calendar;
+window.FullCalendarDayGridPlugin = dayGridPlugin;
+window.FullCalendarTimeGridPlugin = timeGridPlugin;
+window.FullCalendarListPlugin = listPlugin;
+window.FullCalendarInteractionPlugin = interactionPlugin;
