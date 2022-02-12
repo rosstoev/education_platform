8fe09e13522e3ee1a299fcfb74c5d49d16dfc66d
ros <rossto3v@gmail.com>
2022-02-12 19:32:40 +0200

    Create test pages

---
+++ src/Controller/Admin/Teacher/ExaminationTestController.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Admin\Teacher;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route ("/teacher/test", name="teacher_test_")
+ */
+class ExaminationTestController extends AbstractController
+{
+    /**
+     * @Route ("/list", name="list")
+     */
+    public function list(): Response
+    {
+        return $this->render("admin/teacher/pages/examination-test/list.html.twig");
+    }
+
+    /**
+     * @Route ("/new", name="new")
+     */
+    public function create(): Response
+    {
+        return $this->render("admin/teacher/pages/examination-test/manage.html.twig");
+    }
+
+    /**
+     * @Route ("/{test}/edit", name="edit")
+     */
+    public function edit(int $test): Response
+    {
+        return $this->render("admin/teacher/pages/examination-test/manage.html.twig");
+    }
+
+    /**
+     * @Route ("/{test}/delete", name="delete")
+     */
+    public function delete()
+    {
+
+    }
+
+    /**
+     * @Route ("/{test}", name="show")
+     */
+    public function show(int $test): Response
+    {
+        return $this->render("admin/teacher/pages/examination-test/show.html.twig", [
+            'test' => $test
+        ]);
+    }
+}
