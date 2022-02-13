406a0fa17498f439e61377717481b54c64861209
ros <rossto3v@gmail.com>
2022-02-13 19:41:39 +0200

    Create profile pages

--- src/Controller/Admin/Teacher/TeacherController.php
+++ src/Controller/Admin/Teacher/TeacherController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Admin\Teacher;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -15,8 +16,32 @@ class TeacherController extends AbstractController
     /**
      * @Route ("/dashboard", name="dashboard")
      */
-    public function dashboard()
+    public function dashboard(): Response
     {
         return $this->render('admin/teacher/pages/dashboard.html.twig');
+    }
+
+    /**
+     * @Route ("/profile", name="profile")
+     */
+    public function profile(): Response
+    {
+        return $this->render("admin/teacher/pages/profile/show.html.twig");
+    }
+
+    /**
+     * @Route ("/profile/edit", name="profile_edit")
+     */
+    public function editProfile(): Response
+    {
+        return $this->render("admin/teacher/pages/profile/manage.html.twig");
+    }
+
+    /**
+     * @Route ("/profile/delete", name="profile_delete")
+     */
+    public function deleteProfile()
+    {
+
     }
 }
