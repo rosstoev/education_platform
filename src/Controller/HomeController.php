b094b2e214fdf14f74a08a58ef7fa63ca725c3d3
ros <rossto3v@gmail.com>
2022-02-20 19:05:10 +0200

    Create form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace App\Controller;
 
 
+use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,8 +23,18 @@ class HomeController extends AbstractController
     /**
      * @Route ("/register", name="register")
      */
-    public function register(): Response
+    public function register(Request $request): Response
     {
-        return $this->render('pages/register.html.twig');
+        $form = $this->createForm(RegistrationType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+        }
+
+
+        return $this->render('pages/register.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
