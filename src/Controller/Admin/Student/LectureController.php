9733d64896449141311793b35dee2fd4e1f286c1
ros <rossto3v@gmail.com>
2022-02-24 11:55:30 +0200

    Create lecture pages

--- src/Controller/Admin/Student/LectureController.php
+++ src/Controller/Admin/Student/LectureController.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 namespace App\Controller\Admin\Student;
 
 
+use App\Entity\Education\Lecture;
+use App\Form\Student\FilterLectureType;
+use App\Repository\Education\LectureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,15 +20,26 @@ class LectureController extends AbstractController
     /**
      * @Route ("/list", name="list")
      */
-    public function list(): Response
+    public function list(Request $request, LectureRepository $lectureRepo): Response
     {
-        return $this->render("admin/student/pages/lecture/list.html.twig");
+        /** @var \App\Entity\Student $student */
+        $student = $this->getUser();
+        $form = $this->createForm(FilterLectureType::class);
+        $form->handleRequest($request);
+        $formData = $form->getData();
+
+        $lectures = $lectureRepo->findByStudent($student, $formData);
+
+        return $this->render("admin/student/pages/lecture/list.html.twig", [
+            'lectures' => $lectures,
+            'form' => $form->createView()
+        ]);
     }
 
     /**
      * @Route ("/{lecture}", name="show")
      */
-    public function show(int $lecture): Response
+    public function show(Lecture $lecture): Response
     {
 
         return $this->render("admin/student/pages/lecture/show.html.twig", [
--- src/Repository/Education/LectureRepository.php
+++ src/Repository/Education/LectureRepository.php
@@ -7,6 +7,7 @@ use App\Entity\Education\Lecture;
 use App\Entity\Student;
 use App\Entity\Teacher;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -28,15 +29,37 @@ class LectureRepository extends ServiceEntityRepository
             ->leftJoin('lecture.discipline', 'discipline')
             ->where('discipline.teacher = :teacher')
             ->setParameter('teacher', $teacher);
+
+        $this->addFilterCriteria($qb, $filter);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function findByStudent(Student $student, ?LectureFilterDTO $filter)
+    {
+        $qb = $this->createQueryBuilder('lecture')
+        ->leftJoin('lecture.discipline', 'discipline')
+        ->leftJoin('lecture.studentGroup', 'studentGroup')
+        ->leftJoin('studentGroup.students', 'student')
+        ->where('student = :student')
+        ->setParameter('student', $student);
+
+        $this->addFilterCriteria($qb, $filter);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    private function addFilterCriteria(QueryBuilder $qb, ?LectureFilterDTO $filter): void
+    {
         if (!empty($filter)) {
             if (!empty($filter->getDiscipline())) {
                 $qb->andWhere('discipline = :discipline')
-                ->setParameter('discipline', $filter->getDiscipline());
+                    ->setParameter('discipline', $filter->getDiscipline());
             }
 
             if (!empty($filter->getFrom())) {
                 $qb->andWhere('lecture.startDate >= :fromDate')
-                ->setParameter('fromDate', $filter->getFrom()->format('Y-m-d'));
+                    ->setParameter('fromDate', $filter->getFrom()->format('Y-m-d'));
             }
 
             if (!empty($filter->getTo())) {
@@ -44,18 +67,5 @@ class LectureRepository extends ServiceEntityRepository
                     ->setParameter('toDate', $filter->getTo()->format('Y-m-d 23:59:59'));
             }
         }
-
-        return $qb->getQuery()->getResult();
-    }
-
-    public function findByStudent(Student $student)
-    {
-        $qb = $this->createQueryBuilder('l')
-        ->leftJoin('l.studentGroup', 'studentGroup')
-        ->leftJoin('studentGroup.students', 'student')
-        ->where('student = :student')
-        ->setParameter('student', $student);
-
-        return $qb->getQuery()->getResult();
     }
 }
