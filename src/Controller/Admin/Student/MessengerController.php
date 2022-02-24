b8f1944988a60662a304c51f0c64e6f0118d61d4
ros <rossto3v@gmail.com>
2022-02-24 17:57:20 +0200

    Create messenger

--- src/Controller/Admin/Student/MessengerController.php
+++ src/Controller/Admin/Student/MessengerController.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 namespace App\Controller\Admin\Student;
 
 
+use App\Entity\Message;
+use App\Entity\User;
+use App\Form\MessengerType;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,41 +22,81 @@ class MessengerController extends AbstractController
     /**
      * @Route ("/inbox", name="inbox")
      */
-    public function inbox(): Response
+    public function inbox(MessageRepository $messageRepo): Response
     {
+        /** @var \App\Entity\Student $student */
+        $student = $this->getUser();
+        $messages = $messageRepo->findBy(['receiver' => $student]);
 
         return $this->render("admin/messenger/inbox.html.twig", [
-            'student' => true
+            'student' => true,
+            'messages' => $messages
         ]);
     }
 
     /**
      * @Route ("/read/{message}", name="read")
      */
-    public function read(int $message): Response
+    public function read(Message $message, EntityManagerInterface $em): Response
     {
+        /** @var \App\Entity\Student $student */
+        $student = $this->getUser();
+
+        if($student == $message->getReceiver()) {
+            $message->setIsReaded(true);
+            $em->persist($message);
+            $em->flush();
+        }
+
         return $this->render("admin/messenger/read.html.twig", [
-            'student' => true
+            'student' => true,
+            'message' => $message
         ]);
     }
 
     /**
-     * @Route ("/send", name="send_new")
+     * @Route ("/send/{receiver}", name="send_new", defaults={"receiver" :  null})
      */
-    public function sendNew(): Response
+    public function sendNew(Request $request, ?User $receiver, EntityManagerInterface $em): Response
     {
+        /** @var \App\Entity\Student $student */
+        $student = $this->getUser();
+        if ($receiver == $student) {
+            return $this->redirectToRoute('student_messenger_send_new');
+        }
+
+        $form = $this->createForm(MessengerType::class, null, ['receiver' => $receiver]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var \App\Entity\Message $message */
+            $message = $form->getData();
+            !empty($receiver) ? $message->setReceiver($receiver) : null;
+            $message->setSender($student);
+            $em->persist($message);
+            $em->flush();
+            $this->addFlash('success', 'Успешно изпратено съобщение');
+            return $this->redirectToRoute('student_messenger_inbox');
+        }
+
         return $this->render("admin/messenger/new.html.twig", [
-            'student' => true
+            'student' => true,
+            'form' => $form->createView()
         ]);
     }
 
     /**
-     * @Route ("/sended", name="sended")
+     * @Route ("/sent", name="sent")
      */
-    public function sended(): Response
+    public function sent(MessageRepository $messageRepo): Response
     {
-        return $this->render("sent.html.twig", [
-            'student' => true
+        /** @var \App\Entity\Student $student */
+        $student = $this->getUser();
+        $messages = $messageRepo->findBy(['sender' => $student]);
+
+        return $this->render("admin/messenger/sent.html.twig", [
+            'student' => true,
+            'messages' => $messages
         ]);
     }
 }
